b8aa8d114dcc1275a222e8e87c9112fbac6670b7
Khashayar Shomali <shomalikhashayar@gmail.com>
2023-11-08 01:53:38 +0330

    Pagination links.

--- components/Pagination.php
+++ components/Pagination.php
@@ -1,4 +1,4 @@
-<div class="pagination flex justify-center items-center q-gutter-sm" style="margin-bottom:90px">
+<div class="pagination flex justify-center items-center q-gutter-sm q-mb-xl">
 
     <?php
     $paginate_links = paginate_links(
@@ -10,10 +10,16 @@
 
     if ($paginate_links) {
         foreach ($paginate_links as $link) {
+            // Extract the URL from the link using regular expressions
+            preg_match('/href=["\']([^"\']+)["\']/', $link, $matches);
+            $url = $matches[1] ?? ''; // Get the URL from the first match
+    
             if (strpos($link, 'current') !== false) {
+                // If the link is the current page, style it as active
                 echo "<q-btn dense round unelevated class='text-white bg-primary'>$link</q-btn>";
             } else {
-                echo "<q-btn dense round unelevated>$link</q-btn>";
+                // Wrap the button in an anchor tag with the link URL
+                echo "<q-btn href=\"$url\" dense round unelevated>$link</q-btn>";
             }
         }
     }
