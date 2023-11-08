30b862d02732af037e4e1be2f924f2216c44d3ba
Khashayar Shomali <shomalikhashayar@gmail.com>
2023-11-08 04:36:43 +0330

    Bug fix.

--- components/SelfPraiseCard.php
+++ components/SelfPraiseCard.php
@@ -1,41 +1,8 @@
-<q-card class="self-praises-card q-py-md no-shadow q-radius-md">
-  <q-card-section class="column items-center">
-    <q-icon name="o_pattern" size="40px" color="primary"></q-icon>
-    <h3 class="text-h6 no-margin q-pt-md text-weight-900 text-secondary">سال‌ها تجربه در اختیار شماست</h3>
-  </q-card-section>
-</q-card>
-
-<q-card class="self-praises-card q-py-md no-shadow q-radius-md">
-  <q-card-section class="column items-center">
-    <q-icon name="o_kid_star" size="40px" color="primary" /></q-icon>
-    <h3 class="text-h6 no-margin q-pt-md text-weight-900 text-secondary">در وردپرس متخصص هستیم</h3>
-  </q-card-section>
-</q-card>
-
-<q-card class="self-praises-card q-py-md no-shadow q-radius-md">
-  <q-card-section class="column items-center">
-    <q-icon name="o_format_paint" size="40px" color="primary" /></q-icon>
-    <h3 class="text-h6 no-margin q-pt-md text-weight-900 text-secondary">اختصاصی طراحی می‌کنیم</h3>
-  </q-card-section>
-</q-card>
-
-<q-card class="self-praises-card q-py-md no-shadow q-radius-md">
-  <q-card-section class="column items-center">
-    <q-icon name="o_phone_android" size="40px" color="primary" /></q-icon>
-    <h3 class="text-h6 no-margin q-pt-md text-weight-900 text-secondary">واکنشگرا طراحی می‌کنیم</h3>
-  </q-card-section>
-</q-card>
-
-<q-card class="self-praises-card q-py-md no-shadow q-radius-md">
-  <q-card-section class="column items-center">
-    <q-icon name="o_work_history" size="40px" color="primary" /></q-icon>
-    <h3 class="text-h6 no-margin q-pt-md text-weight-900 text-secondary">به موقع تحویل می‌دهیم</h3>
-  </q-card-section>
-</q-card>
-
-<q-card class="self-praises-card q-py-md no-shadow q-radius-md">
-  <q-card-section class="column items-center">
-    <q-icon name="o_supervised_user_circle" size="40px" color="primary" /></q-icon>
-    <h3 class="text-h6 no-margin q-pt-md text-weight-900 text-secondary">تا پایان پروژه مشاورتان هستیم</h3>
+<q-card class="self-praises-card" v-for="n in 6" :key="n">
+  <q-card-section>
+    <div class="column q-gutter-y-md items-center">
+      <q-icon name="o_work_history" size="40px" color="primary" /></q-icon>
+      <h3 class="text-h6 no-margin text-center q-pt-md text-weight-900 text-secondary">به موقع تحویل می‌دهیم</h3>
+    </div>
   </q-card-section>
 </q-card>
