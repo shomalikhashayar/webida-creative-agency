61ee476d3dcf5e9ff98598872da2d1701fca9933
Khashayar Shomali <shomalikhashayar@gmail.com>
2023-12-05 00:53:31 +0330

    searchform gt-md.

--- searchform.php
+++ searchform.php
@@ -33,3 +33,14 @@
         </q-input>
     </q-form>
 </div>
+
+<div class="gt-md">
+    <q-form>
+        <q-input style="width:250px;" v-model="searchBox" name="s" type="search" clearable clear-icon="close"
+            color="primary" dense outlined placeholder="جستجو در بلاگ">
+            <template #prepend>
+                <q-icon color="primary" name="o_search"></q-icon>
+            </template>
+        </q-input>
+    </q-form>
+</div>
