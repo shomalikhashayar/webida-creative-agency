f9931c629323807a25c421713e9df27ebb5fb1f9
FatemehNilchi <fatemehnilchi77@gmail.com>
2023-12-21 03:18:36 +0330

    add 404

---
+++ components/404/desktop/404Content.php
@@ -0,0 +1,13 @@
+<div class="bg-primary notFoundContainer">
+    <div class="container column full-height items-center justify-center">
+        <h2 class="text-h1 no-margin text-center text-weight-900 text-white">
+            صفحه مورد نظر پیدا نشد
+        </h2>
+        <p class="no-letter-spacing text-body1 notFoundText text-center text-weight-400 line-height-sm text-white"  >
+            ممکن است صفحه مورد نظر وجود شما وجود نداشته باشد، شاید به آدرس جدیدی منتقل شده است!
+        </p>
+        <q-btn @click=""   href="<?php echo esc_url(home_url()); ?>" unelevated rounded label="بازگشت به صفحه اصلی"
+            class="webida-secondary text-white text-body1 text-weight-regular items-end" padding="10px 24px">
+        </q-btn>
+    </div>
+</div>
