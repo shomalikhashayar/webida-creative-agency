d0354fd1acff690ae3913d325fdb8eb68edf92ce
Khashayar Shomali <shomalikhashayar@gmail.com>
2023-12-22 16:14:28 +0330

    Change font weight (content-produce).

--- components/content-produce/desktop/WhyWebidaSection.php
+++ components/content-produce/desktop/WhyWebidaSection.php
@@ -13,7 +13,7 @@
                <h4 class="text-h6 no-margin  no-letter-spacing text-weight-900 text-secondary">
                   کنترل کیفیت دو‌مرحله‌ای رایگان
                </h4>
-               <p class="text-body1 no-margin text-weight-700 line-height-sm text-dark ">
+               <p class="text-body1 no-margin text-weight-400 line-height-sm text-dark ">
                   هر محتوایی که در وبیدا نوشته می‌شود، در دو مرحله کنترل کیفیت شده تا محتوای باکیفیتی در نهایت تحویل
                   مشتری داده شود.
                </p>
@@ -27,7 +27,7 @@
                <h4 class="text-h6 no-margin  no-letter-spacing text-weight-900 text-secondary">
                   طبق لحن رسانه و برند شما
                </h4>
-               <p class="text-body1 no-margin text-weight-700 line-height-sm text-dark ">
+               <p class="text-body1 no-margin text-weight-400 line-height-sm text-dark ">
                   لحن محتوای نهایی که به شما تحویل داده می‌شود، کاملا مطابق محل انتشار آن (بلاگ، رسانه و...) خواهد بود و
                   تمام اصول نگارشی در آن رعایت شده است.
                </p>
@@ -41,7 +41,7 @@
                <h4 class="text-h6 no-margin no-letter-spacing text-weight-900 text-secondary">
                   تحویل فوری
                </h4>
-               <p class="text-body1 no-margin text-weight-700 line-height-sm text-dark ">
+               <p class="text-body1 no-margin text-weight-400 line-height-sm text-dark ">
                   از لحظه‌ قبول سفارش تولید محتوای متنی شما توسط نویسنده وبیدا؛ فارغ از موضوع و تعداد کلمات آن، در ۳۶
                   الی ۷۲ ساعت به شما تحویل داده خواهد شد.
                </p>
@@ -55,7 +55,7 @@
                <h4 class="text-h6 no-margin  no-letter-spacing text-weight-900 text-secondary">
                   ضمانت بازگشت وجه
                </h4>
-               <p class="text-body1 no-margin text-weight-700 line-height-sm text-dark ">
+               <p class="text-body1 no-margin text-weight-400 line-height-sm text-dark ">
                   اگر طبق زمان‌بندی معین‌شده محتوا به شما تحویل داده نشود یا از لحاظ کیفیت مطابق با شیوه‌نامه وبیدا
                   نباشد، کل وجه پرداختی بازگردانده خواهد شد.
                </p>
